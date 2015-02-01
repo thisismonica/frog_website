5a20f7cc96dd29b5004fc35d7a761b328b9d37f1
wqr619 <qirongismonica@gmail.com>
2015-01-31 16:49:56 -0800

    Reorganize code to split server/client programming

---
+++ server/extract_functions.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if(!isset($_SESSION['curr_file']))
+{
+    $res['msg'] = "Error: Current filename not set";
+    $res['success'] = false;
+    $res = json_encode($res);
+}else{
+	// Run python script to extract functions
+	$command = escapeshellcmd('python ../Frog/extract_functions.py ');
+	$result = shell_exec($command.$_SESSION['curr_file']);
+	$res = $result;
+}
+echo $res;
+?>
