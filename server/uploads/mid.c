d77bcdee617515dac6c20ad9f2a9e42cd16d7ab5
wqr619 <qirongismonica@gmail.com>
2015-02-12 20:31:24 -0800

    Update console window

--- server/uploads/mid.c
+++ server/uploads/mid.c
@@ -1,19 +1,20 @@
 int mid( int x, int y, int z )
 {
-    int m;
-    m=z;
-    if(y<z)
-        if(x<y)
-            m=y;
-        else if(x<z)
-            m=y;
-        else if(x>y)
-            m=y;
-        else if(x>z)
-            m=x;
-    return m;
+	int m;
+	m=z;
+	if(y<z)
+		if(x<y)
+			m=y;
+		else if(x<z)
+			m=y;
+	else
+		if(x>y)
+			m=y;
+		else if(x>z)
+			m=x;
+	return m;
 }
 int max( int x)
 {
-    return x;
+	return x;
 }
